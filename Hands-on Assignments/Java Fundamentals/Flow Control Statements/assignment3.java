320893df773235d0297832b9802ca23a24146524
Anand More <code_b1ooded@anandmore.gq>
2019-02-15 12:43:25 +0530

    Assignment 3 for Flow Control Statements.

---
+++ Hands-on Assignments/Java Fundamentals/Flow Control Statements/assignment3.java
@@ -0,0 +1,15 @@
+
+public class assignment3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int length=args.length;
+		if(length==0)
+			System.out.println("No values");
+		else {
+			for(int i=0;i<length;i++)
+				System.out.println(args[i]);
+		}
+	}
+
+}
