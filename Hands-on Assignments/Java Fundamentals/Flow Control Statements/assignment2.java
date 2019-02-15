21c8007a3a6ee43b0b86694340b97d222fb0bc93
Anand More <code_b1ooded@anandmore.gq>
2019-02-15 12:43:03 +0530

    Assignment 2 for Flow Control Statements.

---
+++ Hands-on Assignments/Java Fundamentals/Flow Control Statements/assignment2.java
@@ -0,0 +1,13 @@
+
+public class assignment2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=Integer.parseInt(args[0]);
+		if(num%2==0)
+			System.out.println("Even");
+		else
+			System.out.println("Odd");
+	}
+
+}
