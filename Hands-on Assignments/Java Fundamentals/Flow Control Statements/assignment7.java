00ede9b9f0bd620d13249c327d465cd69db3ec7d
Anand More <code_b1ooded@anandmore.gq>
2019-02-15 15:51:37 +0530

    Assignment 7 for Flow Control Statements.

---
+++ Hands-on Assignments/Java Fundamentals/Flow Control Statements/assignment7.java
@@ -0,0 +1,13 @@
+
+public class assignment7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char var='A';
+		if(Character.isLowerCase(var))
+			System.out.println(Character.toUpperCase(var));
+		else
+			System.out.println(Character.toLowerCase(var));
+	}
+
+}
