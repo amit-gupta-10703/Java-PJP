928a85b87695aaa6be7512054f059d5a5fb76296
Anand More <code_b1ooded@anandmore.gq>
2019-02-15 15:50:35 +0530

    Assignment 4 for Flow Control Statements.

---
+++ Hands-on Assignments/Java Fundamentals/Flow Control Statements/assignment4.java
@@ -0,0 +1,14 @@
+
+public class assignment4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char var1='a';
+		char var2='e';
+		if(var1>var2)
+			System.out.println(var2+","+var1);
+		else
+			System.out.println(var1+","+var2);
+	}
+
+}
