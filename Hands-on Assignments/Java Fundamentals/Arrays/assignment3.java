0f40d6c63fe349ca33ef2ee80ff1755576d2929f
Anand More <code_b1ooded@anandmore.gq>
2019-02-16 14:18:10 +0530

    Assignment 3 for Arrays.

---
+++ Hands-on Assignments/Java Fundamentals/Arrays/assignment3.java
@@ -0,0 +1,22 @@
+
+public class assignment3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[]= {1,4,34,56,7};
+		int num=Integer.parseInt(args[0]);
+		int temp=-1;
+		int position=0;
+		for(int i=0;i<5;i++) {
+			if(arr[i]==num) {
+				temp=1;
+				position=i+1;
+			}
+		}
+		if(temp==1)
+			System.out.println(position);
+		else
+			System.out.println(temp);
+	}
+
+}
