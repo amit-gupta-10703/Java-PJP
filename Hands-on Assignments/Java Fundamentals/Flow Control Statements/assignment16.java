9eac1369d7cd8efe9c89f0608e61f379a70f0f65
Anand More <code_b1ooded@anandmore.gq>
2019-02-16 12:09:39 +0530

    Assignment 16 for Flow Control Statements.

---
+++ Hands-on Assignments/Java Fundamentals/Flow Control Statements/assignment16.java
@@ -0,0 +1,19 @@
+
+public class assignment16 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		if(args.length==0)
+			System.out.println("Please enter an integer number");
+		else {
+			int num=Integer.parseInt(args[0]);
+			for(int i=1;i<=num;i++) {
+				for(int j=1;j<=i;j++)
+					System.out.print("*");
+				if(i!=num)
+					System.out.print("\n");
+			}
+		}
+	}
+
+}
