a2a6492e86bbca82466061c93a6b6993373bb28a
Anand More <code_b1ooded@anandmore.gq>
2019-02-17 11:31:09 +0530

    Assignment 2 for Arrays.

--- Hands-on Assignments/Java Fundamentals/Arrays/assignment2.java
+++ Hands-on Assignments/Java Fundamentals/Arrays/assignment2.java
@@ -3,14 +3,14 @@ public class assignment2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int arr[]= {1,2,3,4,5};
+		int[] arr= {1,2,3,4,5};
 		int max=arr[0];
 		int min=arr[0];
-		for(int i=0;i<5;i++) {
-			if(arr[i]>max)
-				max=arr[i];
-			if(arr[i]<min)
-				min=arr[i];
+		for(int i:arr) {
+			if(i>max)
+				max=i;
+			if(i<min)
+				min=i;
 		}
 		System.out.println("Maximum="+max+", Minimum="+min);
 	}
