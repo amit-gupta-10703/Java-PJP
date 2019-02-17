34dfd3f32e9b612b3d325e8aefe4b7595627559b
Anand More <code_b1ooded@anandmore.gq>
2019-02-17 11:31:50 +0530

    Assignment 4 for Arrays.

---
+++ Hands-on Assignments/Java Fundamentals/Arrays/assignment4.java
@@ -0,0 +1,11 @@
+
+public class assignment4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr= {65,66,67,68,69};
+		for(int i:arr)
+			System.out.print((char)i);
+	}
+
+}
