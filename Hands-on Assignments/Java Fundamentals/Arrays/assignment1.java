599a65ec5fd5633a96360ced4908f427aa979d98
Anand More <code_b1ooded@anandmore.gq>
2019-02-17 11:30:41 +0530

    Assignment 1 for Arrays.

--- Hands-on Assignments/Java Fundamentals/Arrays/assignment1.java
+++ Hands-on Assignments/Java Fundamentals/Arrays/assignment1.java
@@ -3,10 +3,10 @@ public class assignment1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int arr[]= {1,2,3,4,5};
+		int[] arr= {1,2,3,4,5};
 		int sum=0;
-		for(int i=0;i<5;i++)
-			sum=sum+arr[i];
+		for(int i:arr)
+			sum=sum+i;
 		int avg=sum/5;
 		System.out.println("Sum="+sum+", Average="+avg);
 	}
