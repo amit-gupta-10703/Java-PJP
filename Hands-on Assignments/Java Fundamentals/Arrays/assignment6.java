ec3bbd3e0bbcbefc14384f77cf4700ea0fabab91
Anand More <code_b1ooded@anandmore.gq>
2019-02-17 11:56:40 +0530

    Assignment 6 for Arrays.

---
+++ Hands-on Assignments/Java Fundamentals/Arrays/assignment6.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+public class assignment6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] arr= {5,4,3,2,1};
+		Arrays.sort(arr);
+		for(int i:arr)
+			System.out.println(i);
+	}
+
+}
